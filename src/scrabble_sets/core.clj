2c666d8a0734a7e868ef6045a5c23ca9db3f4b45
trikitrok <trikitrok@gmail.com>
2016-09-11 03:30:47 +0200

    formatting code

--- src/scrabble_sets/core.clj
+++ src/scrabble_sets/core.clj
@@ -3,33 +3,8 @@
     [clojure.string :as string]))
 
 (def ^:private tile-distribution
-  {"E" 12
-   "A" 9
-   "I" 9
-   "O" 8
-   "N" 6
-   "R" 6
-   "T" 6
-   "L" 4
-   "S" 4
-   "U" 4
-   "D" 4
-   "G" 3
-   "_" 2
-   "B" 2
-   "C" 2
-   "M" 2
-   "P" 2
-   "F" 2
-   "H" 2
-   "V" 2
-   "W" 2
-   "Y" 2
-   "K" 1
-   "J" 1
-   "X" 1
-   "Q" 1
-   "Z" 1})
+  {"E" 12 "A" 9 "I" 9 "O" 8 "N" 6 "R" 6 "T" 6 "L" 4 "S" 4 "U" 4 "D" 4 "G" 3 "_" 2 "B" 2
+   "C" 2 "M" 2 "P" 2 "F" 2 "H" 2 "V" 2 "W" 2 "Y" 2 "K" 1 "J" 1 "X" 1 "Q" 1 "Z" 1})
 
 (defn- display-tiles-frequencies [[freq tiles]]
   (str freq ": " (string/join ", " (map str tiles))))
@@ -61,7 +36,6 @@
     (if (empty? overconsumed-tiles)
       (display (sort-tiles tiles-left-distribution))
       (display-error overconsumed-tiles))))
-
 
 (defn tiles-left [tiles-in-play]
   (->> tile-distribution
