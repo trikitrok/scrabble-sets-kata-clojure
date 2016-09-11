dcb98d60ce2fee7330e59d65dd309e2b3fbbcaee
trikitrok <trikitrok@gmail.com>
2016-09-11 02:45:47 +0200

    shows the tiles that are left in the bag when no tiles are in play

--- src/scrabble_sets/core.clj
+++ src/scrabble_sets/core.clj
@@ -1,1 +1,50 @@
-(ns scrabble-sets.core)
+(ns scrabble-sets.core
+  (:require
+    [clojure.string :as string]))
+
+(def ^:private tile-distribution
+  {"E" 12
+   "A" 9
+   "I" 9
+   "O" 8
+   "N" 6
+   "R" 6
+   "T" 6
+   "L" 4
+   "S" 4
+   "U" 4
+   "D" 4
+   "G" 3
+   "_" 2
+   "B" 2
+   "C" 2
+   "M" 2
+   "P" 2
+   "F" 2
+   "H" 2
+   "V" 2
+   "W" 2
+   "Y" 2
+   "K" 1
+   "J" 1
+   "X" 1
+   "Q" 1
+   "Z" 1})
+
+(defn- display-tiles-frequencies [[freq tiles]]
+  (str freq ": " (string/join " " (map str tiles))))
+
+(defn- display-sorted-tiles [sorted-tiles]
+  (->> sorted-tiles
+       (map display-tiles-frequencies)
+       (string/join "\n")))
+
+(defn- sort-tiles [tiles-in-bag]
+  (map (fn [[freq & [elems]]]
+         (vector freq (sort (map first elems))))
+       (sort-by #(- (key %)) (group-by #(second %) tiles-in-bag))))
+
+(defn tiles-left [tiles-in-play]
+  (-> tile-distribution
+      sort-tiles
+      display-sorted-tiles))
--- test/scrabble_sets/core_test.clj
+++ test/scrabble_sets/core_test.clj
@@ -2,3 +2,25 @@
   (:require
     [midje.sweet :refer :all]
     [scrabble-sets.core :as scrabble]))
+
+(facts
+  "about scrabble"
+
+  (fact
+    "it shows the tiles that are left in the bag
+    in descending order of the quantity of each tile left.
+    In cases where more than one letter has the same quantity remaining,
+    output those letters in alphabetical order, with blank tiles at the end"
+
+    (fact
+      "when no tile are in play"
+
+      (scrabble/tiles-left
+        "") => (str "12: E\n"
+                    "9: A I\n"
+                    "8: O\n"
+                    "6: N R T\n"
+                    "4: D L S U\n"
+                    "3: G\n"
+                    "2: B C F H M P V W Y _\n"
+                    "1: J K Q X Z"))))
